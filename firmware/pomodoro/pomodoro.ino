d1a77e31d8290fe32bb0cf431ff4544892d82cc1
halyapin@gmail.com <halyapin@gmail.com>
2022-02-18 00:14:21 +0300

    comments

--- firmware/pomodoro/pomodoro.ino
+++ firmware/pomodoro/pomodoro.ino
@@ -11,9 +11,9 @@ const int BTN_PIN = 4;
 
 const int LED_SPEED_MS = 700; // led blinking speed
 
-const uint32_t WORK_T_MS = 1500000; // 25 min
-const uint32_t REST_T_MS = 300000; // 5 min
-const uint32_t LONG_REST_T_MS = 1200000; // 20 min
+const uint32_t WORK_T_MS = 1500000; // work time 25 min
+const uint32_t REST_T_MS = 300000; // rest time 5 min
+const uint32_t LONG_REST_T_MS = 1200000; // long rest 20 min
 
 ////
 
